aaed0cc45ff56ca0ebafad4a212fad82a3cacaa3
elLukitas <alufer7440@ieselcaminas.org>
2022-04-26 19:31:15 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
-        int dato=teclado.nextInt();
-        int[] vector =new int[dato];
-        System.out.println("\nVector inicial hasta :"+dato);
+        int numeroMaximo=teclado.nextInt();
+        int[] vector =new int[numeroMaximo];
+        System.out.println("\nVector inicial hasta :"+numeroMaximo);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) {
                 System.out.println();
             }
             System.out.print(i+1+"\t");
         }
-        vector= Primos.generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:"+dato);
+        vector= Primos.generarPrimos(numeroMaximo);
+        System.out.println("\nVector de primos hasta:"+numeroMaximo);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) {
                 System.out.println();
--- src/Primos.java
+++ src/Primos.java
@@ -1,36 +1,36 @@
 public class Primos {
     // Generar números primos de 1 a max
-    public static int[] generarPrimos (int max) {
+    public static int[] generarPrimos (int numeroMaximo) {
         int i,j;
-        if (max >= 2) {
+        if (numeroMaximo >= 2) {
             // Declaraciones
-            int dim = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[dim];
+            int tamanoArray = numeroMaximo + 1; // Tamaño del array
+            boolean[] esPrimo = new boolean[tamanoArray];
             // Inicializar el array
-            for (i=0; i<dim; i++) {
+            for (i=0; i<tamanoArray; i++) {
                 esPrimo[i] = true;
             }
             // Eliminar el 0 y el 1, que no son primos
             esPrimo[0] = esPrimo[1] = false;
             // Criba
-            for (i=2; i<Math.sqrt(dim)+1; i++) {
+            for (i=2; i<Math.sqrt(tamanoArray)+1; i++) {
                 if (esPrimo[i]) {
                     // Eliminar los múltiplos de i
-                    for (j = 2 * i; j < dim; j += i) {
+                    for (j = 2 * i; j < tamanoArray; j += i) {
                         esPrimo[j] = false;
                     }
                 }
             }
             // ¿Cuántos primos hay?
             int cuenta = 0;
-            for (i=0; i<dim; i++) {
+            for (i=0; i<tamanoArray; i++) {
                 if (esPrimo[i]) {
                     cuenta++;
                 }
             }
             // Rellenar el vector de números primos
             int[] primos = new int[cuenta];
-            for (i=0, j=0; i<dim; i++) {
+            for (i=0, j=0; i<tamanoArray; i++) {
                 if (esPrimo[i]) {
                     primos[j++] = i;
                 }
